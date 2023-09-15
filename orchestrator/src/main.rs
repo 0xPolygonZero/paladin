0336df977ffd323999127b387d0b6bc2ebadbc71
cpu <bored@cpu.dev>
2023-09-15 15:50:53 -0700

    refactor orchestrator main to use asserts

--- orchestrator/src/main.rs
+++ orchestrator/src/main.rs
@@ -7,7 +7,6 @@ use paladin::{
     directive::{apply, fold, indexed, lit, map, Evaluator},
     get_runtime,
 };
-use tracing::info;
 mod init;
 
 #[derive(Parser, Debug)]
@@ -26,20 +25,19 @@ async fn main() -> Result<()> {
 
     let concat_input = indexed(['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!']);
     let string_concat = fold(StringConcat, map(CharToString, lit(concat_input)));
+    let result = runtime.evaluate(string_concat).await?;
+    assert_eq!(result, "hello world!".to_string());
 
     let multiplication = fold(
         GenericMultiplication::<i32>::default(),
         lit(indexed([1, 2, 3, 4, 5, 6])),
     );
+    let result = runtime.evaluate(multiplication).await?;
+    assert_eq!(result, 720);
 
-    let result: String = runtime.evaluate(string_concat).await?;
-    info!("{result:?}");
-
-    let result: i32 = runtime.evaluate(multiplication).await?;
-    info!("{result:?}");
-
-    let mult: i32 = runtime.evaluate(apply(MultiplyBy(3), lit(2))).await?;
-    info!("{mult:?}");
+    let multiply_by = apply(MultiplyBy(3), lit(2));
+    let result = runtime.evaluate(multiply_by).await?;
+    assert_eq!(result, 6);
 
     std::future::pending::<()>().await;
     Ok(())
